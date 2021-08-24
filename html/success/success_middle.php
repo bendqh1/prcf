5de784124a2dea47bbb5a60b4cb8a9a744ca00e4
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-08-24 12:22:09 +0700

    Update success_middle.php

--- html/success/success_middle.php
+++ html/success/success_middle.php
@@ -1,7 +1,7 @@
 <div id="prcf_success_middle">
-    <span class="prcf_header_spans">
+    <p class="prcf_header_spans">
         תודה על התעניינות לעבוד עימי,<br>
         לכל פנייה המצריכה את תגובתי אשתדל להשיב בהקדם האפשרי,<br>
         אוסי.
-    </span>
+    </p>
 </div>
