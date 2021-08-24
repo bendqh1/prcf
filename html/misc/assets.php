edb17940b08e5db04cc4bd1fa2b34356c930258c
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-08-24 08:41:40 +0700

    Update assets.php

--- html/misc/assets.php
+++ html/misc/assets.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
-<link rel="stylesheet" href="../frontend/form_mobile_general_structure.css"></link>
-<link rel="stylesheet" href="../frontend/form_nonmobile_general_structure.css"></link>
+<link rel="stylesheet" href="../../frontend/form_mobile_general_structure.css"></link>
+<link rel="stylesheet" href="../../frontend/form_nonmobile_general_structure.css"></link>
 
-<link rel="stylesheet" href="../frontend/success_mobile_general_structure.css"></link>
-<link rel="stylesheet" href="../frontend/success_nonmobile_general_structure.css"></link>
+<link rel="stylesheet" href="../../frontend/success_mobile_general_structure.css"></link>
+<link rel="stylesheet" href="../../frontend/success_nonmobile_general_structure.css"></link>
