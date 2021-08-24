616ee4fab5a9f03953839ac53d75669edec13af4
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-08-24 09:16:54 +0700

    Update form_middle_1.php

--- html/form/components/form_middle_1.php
+++ html/form/components/form_middle_1.php
@@ -2,12 +2,12 @@
     echo $html = <<<HTML
         <div class="prcf_form_middle_1">
             <div class="flex_child_right">
-                <span><label for="prcf_input_name" id="prcf_label_name">שם</label></span><span class="required">*</span>
+                <span><label for="prcf_input_name" id="prcf_label_name" class="prcf_label">שם</label></span><span class="required">*</span>
                 <input type="text" name="name" id="prcf_input_name" class="prcf_item" required>
                 </input> <!-- required property-->
             </div>
             <div class="flex_child_left">
-                <span><label for="prcf_input_topic" id="prcf_label_topic">פנייה בנושא</label></span><span class="required">*</span>
+                <span><label for="prcf_input_topic" id="prcf_label_topic" class="prcf_label">פנייה בנושא</label></span><span class="required">*</span>
                 <select name="topic" id="prcf_input_topic" class="prcf_item">
                     <option value="קידום אתרים">קידום אתרים</option>
                     <option value="בניית אתרים">הדרכת הקמת אתרים</option>
