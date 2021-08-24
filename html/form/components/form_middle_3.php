43c13b1a2e67f9e439fee113c540b3e1a2940e76
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-08-24 09:17:29 +0700

    Update form_middle_3.php

--- html/form/components/form_middle_3.php
+++ html/form/components/form_middle_3.php
@@ -2,11 +2,11 @@
     echo $html = <<<HTML
         <div class="prcf_form_middle_3">
             <div class="flex_child_right">
-                <div><label for="prcf_input_url" id="prcf_label_url">כתובת אתר אינטרנט (אם רלוונטי)</label></div>
+                <div><label for="prcf_input_url" id="prcf_label_url" class="prcf_label">כתובת אתר אינטרנט (אם רלוונטי)</label></div>
                 <input type="text" dir="ltr" name="url" id="prcf_input_url" class="prcf_item"></input>
             </div>
             <div class="flex_child_left">
-                <div><label for="prcf_input_notes" id="prcf_label_notes">הערות (אם רלוונטי)</label></div>
+                <div><label for="prcf_input_notes" id="prcf_label_notes" class="prcf_label">הערות (אם רלוונטי)</label></div>
                 <!-- <textarea rows="5" name="notes" id="prcf_input_notes" class="prcf_item"></textarea> -->
                 <input name="notes" id="prcf_input_notes" class="prcf_item"></input>
             </div>
