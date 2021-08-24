5d250ff62b856ad8ac7ed32b2d8757d3e4b62c2c
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-08-24 08:21:07 +0700

    Update 2_form_middle_2.php

--- html/2_form_middle_2.php
+++ html/2_form_middle_2.php
@@ -3,12 +3,12 @@
         <div class="prcf_form_middle_2">
         <div class="flex_child_right">
                 <span><label for="prcf_input_email" id="prcf_label_email">כתובת אימייל</label></span><span class="required">*</span>
-                <input type="email" dir="ltr" name="email" id="prcf_input_email" class="prcf_generally_any_field" required></input>
+                <input type="email" dir="ltr" name="email" id="prcf_input_email" class="prcf_item" required></input>
                 <!-- required property-->
             </div>
             <div class="flex_child_left">
                 <div><label for="prcf_input_phone" id="prcf_label_phone">מספר טלפון (אם רלוונטי)</label></div>
-                <input type="tel" dir="ltr" name="phone" id="prcf_input_phone" class="prcf_generally_any_field"></input>
+                <input type="tel" dir="ltr" name="phone" id="prcf_input_phone" class="prcf_item"></input>
             </div>
         </div>
     HTML;
