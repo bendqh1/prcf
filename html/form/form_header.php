fc873b701b369e8411ae8340292c6e1b58b4826b
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-08-24 11:37:14 +0700

    Update form_header.php

--- html/form/form_header.php
+++ html/form/form_header.php
@@ -1,6 +1,6 @@
 <div id="prcf_form_header">
-	<h2 class="prcf_form_header_heading">ליצור עימי קשר</h2>
-	<span class="prcf_form_header_span">
+	<h2 class="prcf__header_headings">ליצור עימי קשר</h2>
+	<span class="prcf_header_spans">
 	אנא מלא\י הטופס להלן עם כמה שיותר פרטים כדי שאבין הכי טוב אם ואיך אוכל לסייע; שדות עם כוכבית הם שדות חובה.
 	</span>
 </div>
