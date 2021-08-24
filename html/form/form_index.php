2a9756076547763f04e103f13c550a6adbcc5b6f
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-08-24 08:30:41 +0700

    Update and rename html/0_form_index.php to html/form/form_index.php

--- html/form/form_index.php
+++ html/form/form_index.php
@@ -4,16 +4,16 @@
 		<title>יצירת קשר:טופס</title>
 		<meta charset="utf-8">
 		<?php
-			include '0_variables.php';
+			include 'variables.php';
 		?>
 	</head>
 	<body>
 		<form dir="rtl" lang="he" action="../backend/contact_form.php" method="POST">
 			<div class="prcf_container">
 				<?php
-					include '1_form_header.php';
-					include '1_form_middle.php';
-					include '1_form_footer.php';
+					include 'form_header.php';
+					include 'form_middle.php';
+					include 'form_footer.php';
 				?>
 			</div>
 		</form>
