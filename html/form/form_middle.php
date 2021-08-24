4cd2d45253f8f56fcd00a2adb55e8636d8555787
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-08-24 08:37:09 +0700

    Update form_middle.php

--- html/form/form_middle.php
+++ html/form/form_middle.php
@@ -1,8 +1,8 @@
 <div id="prcf_form_middle">
 	<?php
-		include '2_form_middle_1.php';
-		include '2_form_middle_2.php';
-		include '2_form_middle_3.php';
-		include '2_form_middle_4.php';
+		include 'form_middle_1.php';
+		include 'form_middle_2.php';
+		include 'form_middle_3.php';
+		include 'form_middle_4.php';
 	?>
 </div>
