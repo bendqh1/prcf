f8c0a16bb619f8fd0d87cfad3b2e5a9d9f5e3f13
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-08-24 12:31:38 +0700

    Update success_index.php

--- html/success/success_index.php
+++ html/success/success_index.php
@@ -6,22 +6,24 @@
 		?>
 	</head>
 	<body>
-		<div id="prcf_success_header">
-			<h2 class="prcf_header_headings">הודעתך נשלחה בהצלחה</h2>
-		</div>
-		
-		<div id="prcf_success_middle">
-			<p class="prcf_header_ps">
-				תודה על התעניינות לעבוד עימי,<br>
-				לכל פנייה המצריכה את תגובתי אשתדל להשיב בהקדם האפשרי,<br>
-				אוסי.
-			</p>
-		</div>
-		
-		<div>
-			<?php
-				include '../misc/homepage.php';
-			?>
+		<div dir="rtl" lang="he" class=".prcf_container">
+			<div id="prcf_success_header">
+				<h2 class="prcf_header_headings">הודעתך נשלחה בהצלחה</h2>
+			</div>
+
+			<div id="prcf_success_middle">
+				<p class="prcf_header_ps">
+					תודה על התעניינות לעבוד עימי,<br>
+					לכל פנייה המצריכה את תגובתי אשתדל להשיב בהקדם האפשרי,<br>
+					אוסי.
+				</p>
+			</div>
+
+			<div id="prcf_success_footer">
+				<?php
+					include '../misc/homepage.php';
+				?>
+			</div>
 		</div>
 	</body>
 </html>
