fc16478ff74c5e69d0a26223a039ccdb86cef541
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-08-24 14:41:39 +0700

    Update form_footer.php

--- html/form/form_footer.php
+++ html/form/form_footer.php
@@ -1,3 +1,3 @@
 <div id="prcf_form_footer">
-	<input type="submit" value="שליחה" id="prcf_input_submit_button"></input>
+	<input type="submit" value="שליחה" class="prcf_finalization_buttons"></input>
 </div>
