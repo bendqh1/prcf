8069704d9e31fa21d95230b96b4880413f52267e
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-08-24 09:17:41 +0700

    Update form_middle_4.php

--- html/form/components/form_middle_4.php
+++ html/form/components/form_middle_4.php
@@ -2,11 +2,11 @@
     echo $html = <<<HTML
         <div class="prcf_form_middle_4">
             <div class="flex_child_right">
-                <div><label for="prcf_input_date" id="prcf_label_date">יום רצוי לפגישה מרחוק (אם רלוונטי)</label></div>
+                <div><label for="prcf_input_date" id="prcf_label_date" class="prcf_label">יום רצוי לפגישה מרחוק (אם רלוונטי)</label></div>
                 <input type="date" name="date" id="prcf_input_date" class="prcf_timing_fields"></input>
             </div>
             <div class="flex_child_left">
-                <div><label for="prcf_input_time" id="prcf_label_time">שעה רצויה לפגישה מרחוק (אם רלוונטי)</label></div>
+                <div><label for="prcf_input_time" id="prcf_label_time" class="prcf_label">שעה רצויה לפגישה מרחוק (אם רלוונטי)</label></div>
                 <input type="time" name="time" id="prcf_input_time" class="prcf_timing_fields"></input>
             </div>
         </div>
