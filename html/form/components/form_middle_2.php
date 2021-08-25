e813fb5fbb3e065f284b469ba1e7c230d23dbb49
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-08-25 12:18:30 +0700

    Update form_middle_2.php

--- html/form/components/form_middle_2.php
+++ html/form/components/form_middle_2.php
@@ -1,15 +1,11 @@
-<?php
-    echo $html = <<<HTML
-        <div class="prcf_form_middle_2">
-        <div class="flex_child_right">
-                <span><label for="prcf_input_email" id="prcf_label_email" class="prcf_label">כתובת אימייל</label></span><span class="prcf_required">*</span>
-                <input type="email" dir="ltr" name="email" id="prcf_input_email" class="prcf_item" required></input>
-                <!-- required property-->
-            </div>
-            <div class="flex_child_left">
-                <div><label for="prcf_input_phone" id="prcf_label_phone" class="prcf_label">מספר טלפון (אם רלוונטי)</label></div>
-                <input type="tel" dir="ltr" name="phone" id="prcf_input_phone" class="prcf_item"></input>
-            </div>
-        </div>
-    HTML;
-?>
+<div class="prcf_form_middle_2">
+<div class="flex_child_right">
+        <span><label for="prcf_input_email" id="prcf_label_email" class="prcf_label">כתובת אימייל</label></span><span class="prcf_required">*</span>
+        <input type="email" dir="ltr" name="email" id="prcf_input_email" class="prcf_item" required></input>
+        <!-- required property-->
+    </div>
+    <div class="flex_child_left">
+        <div><label for="prcf_input_phone" id="prcf_label_phone" class="prcf_label">מספר טלפון (אם רלוונטי)</label></div>
+        <input type="tel" dir="ltr" name="phone" id="prcf_input_phone" class="prcf_item"></input>
+    </div>
+</div>
