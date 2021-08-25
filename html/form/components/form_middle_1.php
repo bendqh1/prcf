2968efef8e232dffb83ee2d209e59edb934b788b
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-08-25 12:18:13 +0700

    Update form_middle_1.php

--- html/form/components/form_middle_1.php
+++ html/form/components/form_middle_1.php
@@ -1,19 +1,15 @@
-<?php
-    echo $html = <<<HTML
-        <div class="prcf_form_middle_1">
-            <div class="flex_child_right">
-                <span><label for="prcf_input_name" id="prcf_label_name" class="prcf_label">שם</label></span><span class="prcf_required">*</span>
-                <input type="text" name="name" id="prcf_input_name" class="prcf_item" required>
-                </input> <!-- required property-->
-            </div>
-            <div class="flex_child_left">
-                <span><label for="prcf_input_topic" id="prcf_label_topic" class="prcf_label">פנייה בנושא</label></span><span class="prcf_required">*</span>
-                <select name="topic" id="prcf_input_topic" class="prcf_item">
-                    <option value="שיעור פרטי בקידום אתרים">שיעור פרטי בקידום אתרים</option>
-                    <option value="שיעור פרטי בבניית אתרים">שיעור פרטי בבניית אתרים</option>
-                    <option value="אחר">אחר</option>
-                </select> <!-- required property-->
-            </div>
-        </div>
-    HTML;
-?>
+<div class="prcf_form_middle_1">
+    <div class="flex_child_right">
+        <span><label for="prcf_input_name" id="prcf_label_name" class="prcf_label">שם</label></span><span class="prcf_required">*</span>
+        <input type="text" name="name" id="prcf_input_name" class="prcf_item" required>
+        </input> <!-- required property-->
+    </div>
+    <div class="flex_child_left">
+        <span><label for="prcf_input_topic" id="prcf_label_topic" class="prcf_label">פנייה בנושא</label></span><span class="prcf_required">*</span>
+        <select name="topic" id="prcf_input_topic" class="prcf_item">
+            <option value="שיעור פרטי בקידום אתרים">שיעור פרטי בקידום אתרים</option>
+            <option value="שיעור פרטי בבניית אתרים">שיעור פרטי בבניית אתרים</option>
+            <option value="אחר">אחר</option>
+        </select> <!-- required property-->
+    </div>
+</div>
